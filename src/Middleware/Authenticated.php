888a870ad52a10e322bd0ef2086f86490823ec93
Kresna <satyakresna@unud.ac.id>
2021-11-16 16:14:29 +0800

    improve Authenticated middleware for Laravel 5.5

--- src/Middleware/Authenticated.php
+++ src/Middleware/Authenticated.php
@@ -1,19 +1,48 @@
-<?php
+<?php 
 
 namespace RistekUSDI\SSO\Middleware;
 
-use Illuminate\Auth\Middleware\Authenticate;
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
 
-class Authenticated extends Authenticate
-{
-    /**
-     * Redirect user if it's not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
-     */
-    protected function redirectTo($request)
-    {
-        return route('sso.login');
-    }
+class Authenticated {
+
+	/**
+	 * The Guard implementation.
+	 *
+	 * @var Guard
+	 */
+	protected $auth;
+
+	/**
+	 * Create a new filter instance.
+	 *
+	 * @param  Guard  $auth
+	 * @return void
+	 */
+	public function __construct(Guard $auth)
+	{
+		$this->auth = $auth;
+	}
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if ($this->auth->guest()) {
+			if ($request->ajax()) {
+				return response('Unauthorized.', 401);
+			} else {
+				return redirect()->guest('sso/login');
+			}
+		}
+
+		return $next($request);
+	}
+
 }
