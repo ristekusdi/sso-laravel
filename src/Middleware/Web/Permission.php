4cbf6ccb802af1ed89d2953239b273ba97737f38
Kresna Satya <satyakresna@unud.ac.id>
2022-09-23 12:10:49 +0800

    fix: rename middleware class

--- src/Middleware/Web/Permission.php
+++ src/Middleware/Web/Permission.php
@@ -5,7 +5,7 @@ namespace RistekUSDI\SSO\Middleware\Web;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class Role {
+class Permission {
 
 	/**
 	 * Handle if user have permission(s) based on role active permissions in current client app.
@@ -20,7 +20,7 @@ class Role {
         $permissions = array_unique(array_filter(explode('|', ($permissions[0] ?? ''))));
 		if (! Auth::guard('imissu-web')->hasPermission($permissions)) {
             $permissions_str = implode(', ', $permissions);
-            abort(403, "User dengan ijin {$permissions_str} yang diijinkan mengakses sumber ini!");
+            abort(403, "Pengguna dengan ijin {$permissions_str} yang diijinkan mengakses sumber ini!");
         }
 
         return $next($request);
