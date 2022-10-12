b3258edc6768a557ba1ce04dce9f725ec50d5ee8
wardanaa <33146622+wardanaa@users.noreply.github.com>
2022-10-12 16:25:57 +0800

    fix: fix controller not found

--- stubs/routes/v1/api.php
+++ stubs/routes/v1/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', 'App\Http\Controllers\SSO\Token\AuthController@login')->name('sso.token.login');
-Route::get('/userinfo', 'App\Http\Controllers\SSO\Token\AuthController@userinfo')->middleware('imissu-token')->name('sso.token.userinfo');
+Route::post('/login', 'SSO\Token\AuthController@login')->name('sso.token.login');
+Route::get('/userinfo', 'SSO\Token\AuthController@userinfo')->middleware('imissu-token')->name('sso.token.userinfo');
--- stubs/routes/v1/web.php
+++ stubs/routes/v1/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/sso/login', 'App\Http\Controllers\SSO\Web\AuthController@login')->name('sso.web.login');
-Route::get('/sso/logout', 'App\Http\Controllers\SSO\Web\AuthController@logout')->name('sso.web.logout');
-Route::get('/sso/callback', 'App\Http\Controllers\SSO\Web\AuthController@callback')->name('sso.web.callback');
+Route::get('/sso/login', 'SSO\Web\AuthController@login')->name('sso.web.login');
+Route::get('/sso/logout', 'SSO\Web\AuthController@logout')->name('sso.web.logout');
+Route::get('/sso/callback', 'SSO\Web\AuthController@callback')->name('sso.web.callback');
