8af0fff615c847460d58f280c3ecd3bb8e4d1840
Kresna Satya <kresnasatya72@gmail.com>
2024-01-31 10:41:45 +0800

    update stubs

--- stubs/resources/views/advance.blade.php
+++ stubs/resources/views/advance.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,48 +8,49 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Demo SSO Laravel - Advance</title>
 </head>
+
 <body>
     <h1>SSO Web Demo Advance</h1>
     <a href="{{ route('sso.web.logout') }}">Log out</a>
     <p>List of client roles belongs to {{ auth('imissu-web')->user()->name }}</p>
     <ul>
         @foreach (auth('imissu-web')->user()->client_roles as $role)
-            <li>{{ $role }}</li>
+        <li>{{ $role }}</li>
         @endforeach
     </ul>
     <p>Active role / current role: {{ auth('imissu-web')->user()->role->name ?? '??' }}</p>
     <p>Permissions:</p>
     @php
-        $permissions = auth('imissu-web')->user()->role->permissions ?? [];
+    $permissions = auth('imissu-web')->user()->role->permissions ?? [];
     @endphp
     @if (!empty($permissions))
     <ul>
         @foreach ($permissions as $perm)
-            <li>{{ $perm }}</li>
+        <li>{{ $perm }}</li>
         @endforeach
     </ul>
     @else
-        <p><strong>No permissions available</strong></p>
+    <p><strong>No permissions available</strong></p>
     @endif
-    @if (auth('imissu-web')->user()->hasPermission('manage-settings'))
-    <p>This user has permission manage-settings</p>
+    @if (auth('imissu-web')->user()->hasPermission('user:view'))
+    <p>This user has permission user:view</p>
     @else
-    <p>This user doesn't have permission manage-settings</p>
+    <p>This user doesn't have permission user:view</p>
     @endif
 
-    @if (auth('imissu-web')->user()->hasRole('Developer'))
-    <p>This user has role Developer</p>
+    @if (auth('imissu-web')->user()->hasRole('Pegawai'))
+    <p>This user has role Pegawai</p>
     @else
-    <p>This user doesn't have role Developer</p>
+    <p>This user doesn't have role Pegawai</p>
     @endif
     <form action="">
         <select name="roles" id="roles">
             <option value="0">Roles</option>
             @foreach (auth('imissu-web')->user()->roles as $role)
-                <option value="{{ json_encode($role) }}" {{ (auth('imissu-web')->user()->role->name == $role->name) ? 'selected' : '' }}>{{ $role->name }}</option>
+            <option value="{{ json_encode($role) }}" {{ (auth('imissu-web')->user()->role->name == $role->name) ? 'selected' : '' }}>{{ $role->name }}</option>
             @endforeach
         </select>
-        <input type="hidden" name="current_url" value="{{ url()->current() }}">
+        <input type="hidden" name="home_url" value="{{ url()->current() }}">
     </form>
     <p>Available attributes:</p>
     <table width="100%" style="border: 1px solid black;">
@@ -62,15 +64,15 @@
                 <td style="border: 1px solid black;">{{ $key }}</td>
                 <td style="border: 1px solid black;">
                     @if (is_array($value))
-                        @if (in_array($key, ['roles', 'realm_access', 'resource_access']))
-                            {{ json_encode($value) }}
-                        @else
-                            {{ collect($value)->implode(', ') }}
-                        @endif
-                    @elseif (is_object($value))
-                        {{ json_encode($value) }}
+                    @if (in_array($key, ['roles', 'realm_access', 'resource_access']))
+                    {{ json_encode($value) }}
                     @else
-                        {{ $value }}
+                    {{ collect($value)->implode(', ') }}
+                    @endif
+                    @elseif (is_object($value))
+                    {{ json_encode($value) }}
+                    @else
+                    {{ $value }}
                     @endif
                 </td>
             </tr>
@@ -78,4 +80,5 @@
         </tbody>
     </table>
 </body>
+
 </html>
--- stubs/routes/sso-web-demo.php
+++ stubs/routes/sso-web-demo.php
@@ -6,6 +6,6 @@ Route::middleware(['imissu-web'])->group(function () {
     Route::get('/sso-web-demo', [App\Http\Controllers\SSO\Web\DemoController::class, 'index']);
     Route::get('/sso-web-demo/basic', [App\Http\Controllers\SSO\Web\DemoController::class, 'basic']);
     Route::get('/sso-web-demo/advance', [App\Http\Controllers\SSO\Web\DemoController::class, 'advance']);
-    // Only user that have role active Admin can access!
+    // User with role Admin can access this page!
     Route::get('/sso-web-demo/admin', [\App\Http\Controllers\SSO\Web\DemoController::class, 'admin'])->middleware('imissu-web.role_active:Admin');
 });
